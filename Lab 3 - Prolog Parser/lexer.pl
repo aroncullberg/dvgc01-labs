ef04730cb9f7286a800721be684b259490294067
Hajto0 <viktorhajto0@gmail.com>
2025-05-02 14:24:16 +0200

    stuff

--- Lab 3 - Prolog Parser/lexer.pl
+++ Lab 3 - Prolog Parser/lexer.pl
@@ -1,8 +1,37 @@
-token_code(program, 256).
-token_code(testok1, 1).
-token_code(begin, 2).
+/* Identifiers and literals */
+token_value(id,           270).
+token_value(':=',         271).
+token_value(number,       272).
 
-convert_token(Token, Code) :- token_code(Token, Code), !.
+/* Keywords */
+token_value(program,      256).
+token_value(input,        257).
+token_value(output,       258).
+token_value(var,          259).
+token_value(integer,      260).
+token_value(begin,        261).
+token_value(end,          262).
+
+/* Special compound tokens */
+token_value(assign,       271).
+
+/* Symbols - using ASCII values or custom token codes */
+token_value('(',          40).
+token_value(')',          41).
+token_value(',',          44).
+token_value(';',          59).
+token_value(':',          58).
+token_value('.',          46).
+token_value('+',          43).
+token_value('*',          42).
+token_value('=',          61).
+
+/* Special values */
+token_value(undef,        273).
+token_value(eof,          275).
+token_value(nfound,       -1).
+
+convert_token(Token, Code) :- token_value(Token, Code), !.
 
 % see how 'testread([H|T])' works, its a predicate(?) that can take a list, store the first element in H and the rest of the list in T
 
